66b962eea00aedbacacdd1ac8f8db91be63a152e
ParkSungGyu1 <steven0507@naver.com>
2024-07-01 14:18:29 +0900

    first commit

---
+++ ec2Example/src/main/kotlin/com/example/ec2example/Ec2ExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ec2example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ec2ExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<Ec2ExampleApplication>(*args)
+}
